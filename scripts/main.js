fd41cbdc4d6662809194810853e5587fcacdfa20
BluFedora <r.shareef28@yahoo.com>
2016-11-11 13:37:03 -0500

    More work on website

--- scripts/main.js
+++ scripts/main.js
@@ -43,4 +43,22 @@ function init()
     forEachClass("contactLink",     setHrefFunctor(kContactLink));
 };
 
+function readTextFile(file, callback)
+{
+	var rawFile = new XMLHttpRequest();
+	rawFile.open("GET", file, true);
+	rawFile.onreadystatechange = function ()
+	{
+		if(rawFile.readyState === 4)
+		{
+			if(rawFile.status === 200 || rawFile.status == 0)
+			{
+				var allText = rawFile.responseText;
+				callback(allText);
+			}
+		}
+	}
+	rawFile.send(null);
+}
+
 window.addEventListener("load", init, false);
