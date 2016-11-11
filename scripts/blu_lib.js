838175d09d0c335fcf5ada9b8ddf6946d8d44a1c
BluFedora <r.shareef28@yahoo.com>
2016-11-11 14:29:50 -0500

    Update 3

--- scripts/blu_lib.js
+++ scripts/blu_lib.js
@@ -1,6 +1,4 @@
 (function() {
-	var document = document || null;
-	
 	var EventHandler = function() 
 	{
 		this.events = [];
@@ -143,4 +141,24 @@
 		el.init();
 		return el;
 	};
+    
+    window.blu = $;
+    
+    window.loadFile = function(file, callback)
+    {
+        var rawFile = new XMLHttpRequest();
+        rawFile.open("GET", file, true);
+        rawFile.onreadystatechange = function ()
+        {
+            if(rawFile.readyState === 4)
+            {
+                if(rawFile.status === 200 || rawFile.status == 0)
+                {
+                    var allText = rawFile.responseText;
+                    callback(allText);
+                }
+            }
+        }
+        rawFile.send(null);
+    };
 })();
